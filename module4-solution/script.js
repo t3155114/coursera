46dced13f00b16460dce80c410e09fd5a325c8a4
t3155114 <t3155114@gmail.com>
2016-11-28 20:40:01 -0500

    js2

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -15,13 +15,13 @@ Good Bye Jim */
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (i=0; i<names.length; i++) {
-
+console.log (names[i]);
  var firstLetter = names[i].charAt(0);
 
   if (firstLetter.toLowerCase()=='j' ) {
-       byeSpeaker.speak(names[i]);
+       byeSpeaker.speak();
   } else {
-    helloSpeaker.speak(names[i]);
+    helloSpeaker.speak();
   }
 }
 
